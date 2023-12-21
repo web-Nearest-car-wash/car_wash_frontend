a755efed6a5ccc519669aea190b84324c0263572
Ruslan Lyahov <ruslanlyahov93@yandex.ru>
2023-12-22 05:53:06 +0700

    feat(header): create header

--- src/components/Sections/Header/Header.jsx
+++ src/components/Sections/Header/Header.jsx
@@ -1,7 +1,17 @@
+import React from 'react';
 import styles from './Header.module.css';
+import logo from '../../../assets/logo.svg';
 
 function Header() {
-	return <header className={styles.header}>header</header>;
+	return (
+		<header className={styles.header}>
+			<button className={styles.button}>Показать мойку рядом</button>
+			<img className={styles.logo} alt="logo" src={logo} />
+			<div className={styles.location}>
+				<p className={styles.city}>Москва</p>
+			</div>
+		</header>
+	);
 }
 
 export default Header;
