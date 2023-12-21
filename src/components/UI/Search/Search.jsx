a74caa42ce147278e48ab9391fbb7ab7af380d95
Maxim Novak <maksim.efremof@gmail.com>
2023-12-21 22:26:49 +0300

    refactor(search): intermediate step of renaming components
    Intermediate step of renaming components

--- src/components/UI/Search/Search.jsx
+++ src/components/UI/Search/Search.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Search.module.css';
-import IconSearch from '../icons/IconSearch';
-import RemoveSearch from '../icons/RemoveSearch';
+import IconSearch from '../icons/IconSearch111';
+import RemoveSearch from '../icons/RemoveSearch111';
 import '../../../index.css';
 
 function Search({ query, nightMode, placeholder, onChange, clearInput }) {
