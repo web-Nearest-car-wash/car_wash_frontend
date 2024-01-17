1d4b7cceeea44b4212aa1189ba76665b8f89869b
JayWee <tylykin2013@yandex.ru>
2024-01-17 20:03:49 +0300

    feat(store filters-slice): create filters-slice and filters reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import carWashesReducer from './carWashes/carWashes-slice';
+import filtersReducer from './filters/filters-slice';
 
 const store = configureStore({
 	reducer: {
 		carWashes: carWashesReducer,
+		filters: filtersReducer,
 	},
 });
 
