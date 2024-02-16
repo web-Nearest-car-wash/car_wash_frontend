34d0ca205d184d0724b0afbe44b854bd99a5edb4
Aleksey <1862647@gmail.com>
2024-02-16 19:56:38 +0700

    fix: geolocation message

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -12,7 +12,6 @@ import { handleOpen } from '../../store/filters/filters-slice';
 import Loader from '../../components/UI/Loader/Loader';
 
 function HomePage() {
-	console.log(process.env);
 	const { listCarWashes, loading } = useSelector(selectCarWashes);
 	const [query, setQuery] = useState('');
 	const dispatch = useDispatch();
